fae68ecc6384c862168e232da99ba043e8b643b8
bestalosh <bestalosh@gmail.com>
2026-09-06 14:13:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo -e "\nDownloading x-ui custom release from GitHub..."
 
 # دانلود فایل زیپ از بخش Release شما
-wget -N --no-check-certificate -O x-ui-custom.zip "https://github.com/bestalosh/3x-ui/releases/download/v1.0/x-ui-custom-release.zip"
+wget -N --no-check-certificate -O x-ui.zip "https://github.com/bestalosh/3x-ui/releases/download/v1.0/x-ui.zip"
 
 if [[ $? -ne 0 ]]; then
     echo "Failed to download the release. Check the URL or make sure the repo is Public."
@@ -20,11 +20,11 @@ read -p "Choose option [1 or 2] (Default: 2): " core_choice
 
 if [[ "$core_choice" == "1" ]]; then
     echo "Preparing Standard Core..."
-    mv bin/xray-standard bin/xray-linux-amd64
+    mv bin/xray-standard bin/xray-linux-arm64
     rm -f bin/xray-custom
 else
     echo "Preparing Custom Protected Core..."
-    mv bin/xray-custom bin/xray-linux-amd64
+    mv bin/xray-custom bin/xray-linux-arm64
     rm -f bin/xray-standard
 fi
 
@@ -32,5 +32,5 @@ chmod +x x-ui
 ./x-ui install
 
 cd ..
-rm -rf x-ui-custom.zip x-ui
+rm -rf x-ui.zip x-ui
 echo -e "\nInstallation finished successfully."
